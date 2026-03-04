34e8b8bf253ade5668114a2554ccfefd3e758680
NIkhildutt7674 <anikhildutt@gmail.com>
2026-03-04 05:55:42 -0800

    Inital commit

---
+++ hello.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+print("hello")
+a=10
+b=a*11
+print(a,b)
---
+++ library/numpy/untitled1.py
@@ -0,0 +1,9 @@
+import numpy as np
+list1=[1,2,3,4,5,6]
+arr=np.array(list1)
+list2=[10,20,30,50,60,70]
+arr1=np.array(list2)
+print(np.add(arr,arr1))
+print(np.subtract(arr1,arr))
+print(np.multiply(arr1,arr))
+print(np.divide(arr1,arr))
